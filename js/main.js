341764652db435200657e4f57bf5b3e470d02ab6
Hyojin <89653508+parkthoven88@users.noreply.github.com>
2021-12-13 00:09:29 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -209,7 +209,7 @@ async function  createPosts (posts) {
         let p4 = createElemWithText("p", `${author.company.catchPhrase}`);
 
         let button = createElemWithText("button", 'Show Comments');
-        button.dataset.posts = posts.id;
+        button.dataset.posts = posts[i].id;
 
         let section = await displayComments (posts[i].id);
 
